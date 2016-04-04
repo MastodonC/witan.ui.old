5c082072c7ea48949ad948c8b172707c0ce963d0
Antony Woods <antony@mastodonc.com>
2016-04-04 17:29:55 +0100

    Version 0.3.11-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject witan.ui "0.3.10"
+(defproject witan.ui "0.3.11-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
